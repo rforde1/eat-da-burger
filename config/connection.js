84ec5dbb44c021afdd0b10f56e12da72ff5bde8d
ramiforde <ramiforde@gmail.com>
2020-05-14 19:13:35 -0400

    Procfile

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,23 @@
 // Databases
 
   let mysql = require("mysql");
-  var connection;
-
-  if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection({
+//  Connection
+  var connection = mysql.createConnection({
       port: 3306,
       host: "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       user: "yqpfsrgow17en3zq",
       password: "gycj16ot7w9qh7oo",
       database: "qv9ug5ojib8ee60o"
     });
-  } else {
-    connection = mysql.createConnection({
-      port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burger_db"
-    });
-  }
+  // } else {
+  //   connection = mysql.createConnection({
+  //     port: 3306,
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "password",
+  //   database: "burger_db"
+  //   });
+  // }
 
 
 //Connection to host 
